e9bdf09569318c445e4fb6f4f679376a481c80fd
Moongazing <tunali_2@outlook.com>
2022-11-30 09:53:43 +0300

    Bussiness

---
+++ TAO.MR.Business/Abstract/IMemberService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO.MR.Entities.Concrete;
+using TAO_Core.Utilities.Results;
+
+namespace TAO.MR.Business.Abstract
+{
+  public interface IMemberService
+  {
+    IResult Add(Member member);
+  }
+}
---
+++ TAO.MR.Business/Concrete/MemberManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO.MR.Business.Abstract;
+using TAO.MR.DataAccess.Abstract;
+using TAO.MR.Entities.Concrete;
+using TAO_Core.Utilities.Results;
+
+namespace TAO.MR.Business.Concrete
+{
+  public class MemberManager :IMemberService
+  {
+    IMemberDal _memberDal;
+    public MemberManager(IMemberDal memberDal)
+    {
+      _memberDal = memberDal;
+    }
+
+    public IResult Add(Member member)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
