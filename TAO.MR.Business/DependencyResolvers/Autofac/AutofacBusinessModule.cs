10f3238c235b1ea2697f3a10b93b2a9adc8c49ae
Moongazing <tunali_2@outlook.com>
2022-11-30 11:24:11 +0300

    Ioc

---
+++ TAO.MR.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO.MR.Business.Abstract;
+using TAO.MR.Business.Concrete;
+using TAO.MR.Business.ServiceAdapters;
+using TAO.MR.DataAccess.Abstract;
+using TAO.MR.DataAccess.Concrete.EntityFramework;
+
+namespace TAO.MR.Business.DependencyResolvers.Autofac
+{
+  public class AutofacBusinessModule : Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<MemberManager>().As<IMemberService>().SingleInstance();
+      builder.RegisterType<EfMemberDal>().As<IMemberDal>().SingleInstance();
+
+      builder.RegisterType<IKpsService>().As<KpsServiceAdapter>().SingleInstance();
+    }
+  }
+}
