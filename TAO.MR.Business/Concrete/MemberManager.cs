b1e7d36fda4d520df1537ea48fb7e83432a2fb98
Moongazing <tunali_2@outlook.com>
2022-11-30 11:17:18 +0300

    Adding Fluent Validation

--- TAO.MR.Business/Concrete/MemberManager.cs
+++ TAO.MR.Business/Concrete/MemberManager.cs
@@ -5,8 +5,10 @@ using System.Text;
 using TAO.MR.Business.Abstract;
 using TAO.MR.Business.Constants;
 using TAO.MR.Business.ServiceAdapters;
+using TAO.MR.Business.ValidationRules.FluentValidation;
 using TAO.MR.DataAccess.Abstract;
 using TAO.MR.Entities.Concrete;
+using TAO_Core.Aspects.Autofac.Validation;
 using TAO_Core.Utilities.Results;
 using TAO_Core.Utilities.Results.Concrete;
 
@@ -21,7 +23,7 @@ namespace TAO.MR.Business.Concrete
       _memberDal = memberDal;
       _kpsService = kpsService; 
     }
-
+    [ValidationAspect(typeof(MemberValidatior))]
     public IResult Add(Member member)
     {
       if(_kpsService.ValidateUser(member) == false )
---
+++ TAO.MR.Business/ValidationRules/FluentValidation/MemberValidatior.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO.MR.Entities.Concrete;
+using TAO_Core.Aspects.Autofac.Validation;
+
+namespace TAO.MR.Business.ValidationRules.FluentValidation
+{
+  public class MemberValidatior: AbstractValidator<Member>
+  {
+    public MemberValidatior()
+    {
+      #region NotEmptyRules
+
+      RuleFor(m => m.FirstName).NotEmpty();
+      RuleFor(m => m.LastName).NotEmpty();
+      RuleFor(m => m.NationalIdentity).NotEmpty();
+      RuleFor(m => m.DateOfBirth).NotEmpty();
+      RuleFor(m => m.Email).NotEmpty();
+
+      #endregion
+
+      RuleFor(m => m.DateOfBirth).LessThan(DateTime.Now);
+      RuleFor(m => m.Email).EmailAddress();
+      RuleFor(m => m.NationalIdentity).Length(11); //Because TCKN length should be 11
+
+    }
+  }
+}
